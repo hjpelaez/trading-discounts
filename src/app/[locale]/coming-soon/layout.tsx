f4027d8c88b01757d9997c0296cad3a257892576
hjpel <hjpelaezcasas@hotmail.com>
2026-01-25 15:12:42 +0100

    Isolate Coming Soon layout to remove Navbar/Footer

---
+++ src/app/[locale]/coming-soon/layout.tsx
@@ -0,0 +1,11 @@
+export default function ComingSoonLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="bg-slate-950 min-h-screen">
+            {children}
+        </div>
+    )
+}
